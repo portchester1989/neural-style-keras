9f4f8124cdaaf02719063e557d16bc35c6a185da
portchester1989 <ken.hayashima1989@gmail.com>
2018-12-13 18:08:24 +0000

    incorporate changes in keras codes

--- model.py
+++ model.py
@@ -21,8 +21,9 @@ def conv(x, n_filters, kernel_size=3, stride=1, relu=True, nb_classes=1, targets
     '''
     if not kernel_size % 2:
         raise ValueError('Expected odd kernel size.')
-    pad = (kernel_size - 1) / 2
-    o = ReflectionPadding2D(padding=(pad, pad))(x)
+    pad = int((kernel_size - 1) / 2)
+    print(pad)
+    o = ReflectionPadding2D(padding=((pad,pad),(pad,pad)))(x)
     o = Conv2D(n_filters, kernel_size, kernel_size,
                       subsample=(stride, stride), init=weights_init)(o)
     # o = BatchNormalization()(o)
